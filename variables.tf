f62c53b8836df5f0c66e71f27f6d7a6d4eebb221
Cédric Derue <cedric.derue@gmail.com>
2024-06-10 17:12:28 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,6 @@ variable "container_name_prefix" {
 variable "image" {
   type        = string
   description = "Container image to deploy. Should be of the form repoName/imagename:tag for images stored in public Docker Hub, or a fully qualified URI for other registries. Images from private registries require additional registry credentials."
-  default     = "mcr.microsoft.com/azuredocs/aci-helloworld"
 }
 
 variable "port" {
